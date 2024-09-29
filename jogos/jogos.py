c55676c0cd9f6f032029b187dbe11166db83413d
ViniMorei <vinicius.moreira.ara@gmail.com>
2024-09-29 17:22:12 -0400

    Filtragem dos jogos

--- jogos/jogos.py
+++ jogos/jogos.py
@@ -4,19 +4,11 @@ lista_jogos = [
         "link":"https://store.playstation.com/pt-br/product/UP0700-CUSA03388_00-DARKSOULS3000000"
     },
     {
-        "nome":"Final Fantasy VII Rebirth",
-        "link":"https://store.playstation.com/pt-br/product/UP0082-PPSA08666_00-0783148264580163"
-    },
-    {
         "nome":"Resident Evil 4 Gold Edition",
         "link":"https://store.playstation.com/pt-br/product/UP0102-PPSA07411_00-RE4GOLDEDITION00"
     },
     {
         "nome":"Horizon Forbidden West",
         "link":"https://store.playstation.com/pt-br/product/UP9000-CUSA28561_00-FORBIDDENWESTPS4"
-    },
-    {
-        "nome":"The Last of Us Part II Deluxe Edition",
-        "link":"https://store.playstation.com/pt-br/product/UP9000-CUSA07820_00-THELASTOFUSP2DLX"
     },
 ]
