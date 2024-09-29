9df56de9e303b13b2ad42ff50324d65d0255b410
ViniMorei <vinicius.moreira.ara@gmail.com>
2024-09-29 13:35:03 -0400

    Definição do histórico de preços

---
+++ precos_historicos/precos.py
@@ -0,0 +1,22 @@
+dict_precos = {
+    {   # //*[@id="UjZIQgueFImpGvgzK5a8Kr2iz9kmAi4T"]
+        "nome":"Dark Souls III",
+        "link":"https://psprices.com/region-br/game/147847/dark-soulstm-iii"
+    },
+    {   # //*[@id="UjZIQgueFImpGvgzK5a8Kr2iz9kmAi4T"]
+        "nome":"Final Fantasy VII Rebirth",
+        "link":"https://psprices.com/region-br/game/6508688/final-fantasy-vii-rebirth"
+    },
+    {   # //*[@id="UjZIQgueFImpGvgzK5a8Kr2iz9kmAi4T"]
+        "nome":"Resident Evil 4 Gold Edition",
+        "link":"https://psprices.com/region-br/game/6925613/resident-evil-4-gold-edition-ps4-e-ps5"
+    },
+    {   # //*[@id="UjZIQgueFImpGvgzK5a8Kr2iz9kmAi4T"]
+        "nome":"Horizon Forbidden West",
+        "link":"https://psprices.com/region-br/game/4809463/horizon-forbidden-westtm"
+    },
+    {   # //*[@id="UjZIQgueFImpGvgzK5a8Kr2iz9kmAi4T"]
+        "nome":"The Last of Us Part II Deluxe Edition",
+        "link":"https://psprices.com/region-br/game/3230147/the-last-of-us-part-ii-digital-deluxe-edition"
+    },
+}
